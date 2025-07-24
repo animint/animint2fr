9a8fecb0efa7af74df022b768961f6e5076ad889
Toby Dylan Hocking <toby.hocking@r-project.org>
2025-07-24 07:04:39 -0400

    add NEWS, more columns

---
+++ NEWS.md
@@ -0,0 +1,9 @@
+## Modifications dans 2025.7.23
+
+- rajouter colonnes dans `BanqueMondiale` : `pays`, `PIB.par.habitant.USD`, `alphabétisation`, `revenu`.
+
+## Modifications dans 2025.7.21
+
+- début : 1è soumission au CRAN.
+- `data(BanqueMondiale)` traduit quelques noms de colonnes de `data(WorldBank, package="animint2")` : `région`, `espérance.de.vie`, `taux.de.fertilité`, `année`.
+
--- inst/translate.R
+++ inst/translate.R
@@ -1,6 +1,6 @@
 data(WorldBank, package="animint2")
-WorldBank$espérance.de.vie <- WorldBank$life.expectancy
-WorldBank$taux.de.fertilité <- WorldBank$fertility.rate
+BanqueMondiale <- WorldBank
+BanqueMondiale$Region <- sub(" \\(.*", "", BanqueMondiale$region)
 en2fr.region <- c(
   "East Asia & Pacific"="Asie de l'est et pacifique",
   "Europe & Central Asia"="Europe et Asie centrale",
@@ -9,12 +9,24 @@ en2fr.region <- c(
   "North America"="Amérique du nord",
   "South Asia"="Asie du sud",
   "Sub-Saharan Africa"="Afrique subsaharienne")
-WorldBank$Region <- sub(" \\(.*", "", WorldBank$region)
-WorldBank$région <- factor(WorldBank$Region, names(en2fr.region), en2fr.region)
-WorldBank$année <- WorldBank$year
-WorldBank$pays <- WorldBank$country
-BanqueMondiale <- WorldBank
+BanqueMondiale$région <- factor(BanqueMondiale$Region, names(en2fr.region), en2fr.region)
+BanqueMondiale$espérance.de.vie <- BanqueMondiale$life.expectancy
+BanqueMondiale$taux.de.fertilité <- BanqueMondiale$fertility.rate
+BanqueMondiale$année <- BanqueMondiale$year
+BanqueMondiale$pays <- BanqueMondiale$country
+BanqueMondiale$PIB.par.habitant.USD <- BanqueMondiale$GDP.per.capita.Current.USD
+BanqueMondiale$alphabétisation <- BanqueMondiale[["15.to.25.yr.female.literacy"]]
+dput(levels(BanqueMondiale$income))
+en2fr.income <- c(
+  "High income: OECD"="Revenu élevé : OCDE",
+  "High income: nonOECD"="Revenu élevé : nonOCDE",
+  "Upper middle income"="Revenu moyen élevé",
+  "Lower middle income"="Revenu moyen bas",
+  "Low income"="Revenu bas",
+  "Not classified"="Pas classé")
+BanqueMondiale$revenu <- factor(BanqueMondiale$income, names(en2fr.income), en2fr.income)
 save(BanqueMondiale, file="../data/BanqueMondiale.RData", compress="xz")
 if(FALSE){
   prompt(BanqueMondiale, "../man/BanqueMondiale.Rd")
 }
+names(BanqueMondiale)
